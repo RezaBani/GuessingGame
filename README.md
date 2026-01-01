5fdd85f65c4fe15aa7ac092a25c1d046ee0f5e82
RezaBanivakil <rezabaniv@gmail.com>
2026-01-01 21:18:41 +0330

    Refactor Repos
    Moved game Logic to main repo and removed extra repo.
    Versioning changed to git tags.

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
-# Guessing_Game
+# GuessingGame
 
 
-**A Guessing game implemented in Rust + Zig using these for educational purpose:**
+## A Guessing game implemented in Rust + Zig using these for educational purpose:
 
-+ generics
-+ comptime
-+ build options
++ **generics**
++ **comptime**
++ **build options**
+
+
+this is a simple guessing game inspired y the rust lang book with only 2 diffrence:
+
+1. random number generated via zig that is hosted in this repo:
+
+https://github.com/RezaBani/random_number_generator
+
+your folder structure should be something like this:
+
+..Parent Folder
+....random_number_generator
+
+in fact you can easily setup this:
+
+`$ git clone --recurse-submodules https://github.com/RezaBani/guessing_game`
+
+change directory to the game and run it with cargo:
+
+`& cd guessing_game`
+
+`$ cargo run`
+
+Note: it will take some time to run for the first time since it's building it's dependency as well.
+
+But source code is more beautiful than the game itself :)
---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use std::{cmp, env, fmt::Debug, io, process::Command, str::FromStr};
+
+fn main() {
+    type NumberType = u8;
+    let secret_number = generate_random_number::<NumberType>();
+
+    println!("Guess the number! (Type q to quit eartly)");
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed To read the input");
+
+        let guess: NumberType = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(err) => match err.kind() {
+                std::num::IntErrorKind::PosOverflow => {
+                    println!(
+                        "Input is too big, max allowed number is: {} . Please try again:",
+                        NumberType::MAX
+                    );
+                    continue;
+                }
+                std::num::IntErrorKind::NegOverflow => {
+                    println!(
+                        "Input is too small, min allowed number is: {} . Please try again:",
+                        NumberType::MIN
+                    );
+                    continue;
+                }
+                std::num::IntErrorKind::InvalidDigit
+                | std::num::IntErrorKind::Zero
+                | std::num::IntErrorKind::Empty
+                | _ => {
+                    if guess.trim().to_lowercase() == "q" {
+                        break;
+                    }
+                    println!("Input is not valid. Please try again:");
+                    continue;
+                }
+            },
+        };
+
+        match guess.cmp(&secret_number) {
+            cmp::Ordering::Less => println!("Guess too small"),
+            cmp::Ordering::Equal => {
+                println!("Guess correct");
+                break;
+            }
+            cmp::Ordering::Greater => println!("Guess too large"),
+        }
+    }
+}
+
+fn generate_random_number<T: std::str::FromStr>() -> T
+where
+    <T as FromStr>::Err: Debug,
+{
+    let rust_cwd = env::current_dir().unwrap();
+    env::set_current_dir("./random_number_generator/").expect("Change cwd failed");
+    let mut zig = Command::new("zig");
+    let output = zig
+        .args(vec![
+            "build",
+            "run",
+            format!("-DUserType={}", std::any::type_name::<T>()).as_str(),
+        ])
+        .output()
+        .unwrap();
+    let number = String::from_utf8(output.stderr).unwrap();
+    let number = number.trim();
+    env::set_current_dir(rust_cwd).unwrap();
+    number.parse().unwrap()
+}
